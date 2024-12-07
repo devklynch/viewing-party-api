e44abd4971dff33606a32158ac564139c387b9df
devklynch <devklynch@gmail.com>
2024-12-06 17:43:14 -0700

    feat and tests for adding an invitee-happy path only

--- spec/requests/api/v1/viewing_parties_request_spec.rb
+++ spec/requests/api/v1/viewing_parties_request_spec.rb
@@ -1,23 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "Viewing Party" do
+    before :each do
+        @user = create(:user)
+        @user1 = create(:user)
+        @user2 = create(:user)
+        @user3= create(:user)
+    end
     describe "Create Viewing Party Endpoint" do
-        it "can create a new viewing party" do
-            @user = create(:user)
-            @user1 = create(:user)
-            @user2 = create(:user)
-            @user3= create(:user)
-            
-            party_params = {
-                name: "Test 6",
-                start_time: "2025-02-02 10:30:00",
-                end_time: "2025-02-02 14:30:00",
-               movie_id: 278,
-                movie_title: "The Shawshank Redemption",
-                invitees: [@user.id,@user2.id]
-            }
+        it "can create a viewing party" do
+    
+        party_params = {
+            name: "Test 6",
+            start_time: "2025-02-02 10:30:00",
+            end_time: "2025-02-02 14:30:00",
+            movie_id: 278,
+            movie_title: "The Shawshank Redemption",
+            invitees: [@user.id,@user2.id]
+        }
 
           post "/api/v1/viewing_parties/#{@user1.id}", params: party_params
+
+
 
             json_response = JSON.parse(response.body, symbolize_names: true)
             #binding.pry
@@ -38,8 +42,35 @@ RSpec.describe "Viewing Party" do
             expect(json_response[:data][:attributes][:attendees][0][:id]).to eq(@user1.id)
             expect(json_response[:data][:attributes][:attendees][1][:id]).to eq(@user.id)
             expect(json_response[:data][:attributes][:attendees][2][:id]).to eq(@user2.id)
+        end
 
+        it "can add new invitees" do
+            party_params = {
+                name: "Test 6",
+                start_time: "2025-02-02 10:30:00",
+                end_time: "2025-02-02 14:30:00",
+                movie_id: 278,
+                movie_title: "The Shawshank Redemption",
+                invitees: [@user.id,@user2.id]
+            }
+            post "/api/v1/viewing_parties/#{@user1.id}", params: party_params
+            party_response = JSON.parse(response.body, symbolize_names: true)
+            viewing_party_id = party_response[:data][:id]
+            #binding.pry
+            invitee_param = {
+                invitees_user_id: @user3.id
+            }
 
+            post "/api/v1/viewing_parties/add_attendee/#{viewing_party_id}", params: invitee_param
+
+            json_response = JSON.parse(response.body, symbolize_names: true)
+            #binding.pry
+            expect(response).to be_successful
+            expect(json_response[:data][:attributes][:attendees][-1][:id]).to eq(@user3.id)
+            expect(json_response[:data][:attributes][:attendees][-1][:name]).to eq(@user3.name)
+            expect(json_response[:data][:attributes][:attendees][-1][:username]).to eq(@user3.username)
+            expect(json_response[:data][:attributes][:attendees][-1][:is_host]).to eq(false)
         end
     end
+
 end
