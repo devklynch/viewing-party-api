c3715cbac74c798a4ab5c70a275728aa5dcb66d7
devklynch <devklynch@gmail.com>
2024-12-07 14:17:16 -0700

    features only for movie and user details

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,13 +1,51 @@
 class Movie
     attr_reader :id,
                 :title,
-                :vote_average
+                :release_year,
+                :vote_average,
+                :runtime,
+                :genres,
+                :summary,
+                :cast,
+                :total_reviews,
+                :reviews
 
-    def initialize(movie_json)
-        movie_json[:results].map do |movie|
-            @id = movie[:id]
-            @title = movie[:title]
-            @vote_average = movie[:vote_average]
+    def initialize(details,credits,reviews)
+            
+            @id = details[:id].to_s
+            @title = details[:title]
+            @release_year = details[:release_date][0..3].to_i
+            @vote_average = details[:vote_average]
+            @runtime = "#{details[:runtime]} minutes"
+            @genres = details[:genres].map do |genre|
+                genre[:name]
+            end
+            @summary = details[:overview]
+            @cast = get_cast(credits[:cast])
+            @total_reviews = reviews[:total_results]
+            @reviews = get_reviews(reviews[:results])
+        
+    end
+
+    def get_cast(cast)
+        cast_list = cast[0..9]
+        cast_list.map do |member|
+            {
+            character: member[:character],
+            actor: member[:name]
+        }
         end
     end
+
+    def get_reviews(reviews)
+        review_list = reviews[0..4]
+        review_list.map do |review|
+            {
+            author: review[:author],
+            review: review[:content]
+        }
+        end
+
+    end
+
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -16,4 +16,25 @@ class MovieSerializer
           end
       }
     end
+
+    def  self.format_movie_details(movie)
+        { data:
+            {
+                id: movie.id,
+                type: "movie",
+                attributes: {
+                    title: movie.title,
+                    release_year: movie.release_year,
+                    vote_average: movie.vote_average,
+                    runtime: movie.runtime,
+                    genres: movie.genres,
+                    summary: movie.summary,
+                    cast: movie.cast,
+                    total_reviews: movie.total_reviews,
+                    reviews: movie.reviews
+                }
+            }
+
+        }
+    end
   end
